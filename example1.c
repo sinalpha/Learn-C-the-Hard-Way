6b8e3fc1dfcaa5436c22657b370fa936c2307137
sinalpha <hanhdhx65@gmail.com>
2021-01-05 22:51:58 +0900

    commit

--- example1.c
+++ example1.c
@@ -5,14 +5,16 @@ int main(int argc, char *argv[])
 
 	int a[10][10];
 	int i = 0;	
+	
+	int count = sizeof(a[0]) / sizeof(int);
 
-	for (int y = 0; y < 10; y++) 
-		for (int x = 0; x < 10; x++) 
+	for (int y = 0; y < count; y++) 
+		for (int x = 0; x < count; x++) 
 			a[x][y] = i++;
 	
 	int *p = a[2];
 
-	for (int x = 0; x < 10; x++)
+	for (int x = 0; x < count; x++)
 		printf("%d ",p[x]);
 	
 	return 0;
